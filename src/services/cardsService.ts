681990325ad0b37c802835180b15e8cf1e6c48df
antcarlosrbj <antoniocarlosrbj@gmail.com>
2022-07-11 16:17:06 -0300

    FIX(cardsService.ts): encrypt CVC

--- src/services/cardsService.ts
+++ src/services/cardsService.ts
@@ -1,5 +1,6 @@
 import { faker } from '@faker-js/faker';
 import dayjs from 'dayjs';
+import Cryptr from 'cryptr';
 
 import * as cardRepository from "../repositories/cardRepository.js";
 import * as companyRepository from "../repositories/companyRepository.js";
@@ -37,17 +38,20 @@ export async function createCard(employeeId, cardType) {
   const employee = await employeeRepository.findById(employeeId);
 
   const cardholderName = generator.cardholderName(employee.fullName);
+
+  const cryptr = new Cryptr(process.env.SECRET);
+  const securityCode = cryptr.encrypt(faker.finance.creditCardCVV());
   
   cardRepository.insert({
     employeeId: employeeId,
     number: faker.finance.creditCardNumber('#{16}'),
     cardholderName: cardholderName,
-    securityCode: faker.finance.creditCardCVV(),
+    securityCode: securityCode,
     expirationDate: dayjs().add(5, 'year').format('MM/YY'),
     password: null,
     isVirtual: false,
     originalCardId: null,
-    isBlocked: true,
+    isBlocked: false,
     type: cardType
   })
 
